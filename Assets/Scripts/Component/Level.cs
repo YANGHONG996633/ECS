28e34069ebb133b2b906a3df29821c94dc6c41c6
yh <309003879@qq.com>
2022-03-28 15:40:32 +0800

    初始化

---
+++ Assets/Scripts/Component/Level.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+
+//IComponent用于实现通用接口
+public class Level : IComponentData
+{
+    public int level;
+}
---
+++ Assets/Scripts/Testing.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Collections;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+using Random = UnityEngine.Random;
+
+public class Testing : MonoBehaviour
+{
+    void Start()
+    {
+        EntityManager entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
+        EntityArchetype entityArchetype = entityManager.CreateArchetype(typeof(Level), typeof(Translation));
+        NativeArray<Entity> entities=new NativeArray<Entity>(10,Allocator.Temp);
+        entityManager.CreateEntity(entityArchetype, entities);
+        for (int i = 0; i < entities.Length; i++)
+        {
+            entityManager.SetComponentData(entities[i],new Translation{Value=new float3(Random.Range(0,100),Random.Range(0,100),Random.Range(0,100))});
+        }
+    }
+
+}
